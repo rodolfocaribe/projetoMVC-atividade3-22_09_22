b4fe440b8044a47b875d6216897bd14b25eca598
RODOLFO <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-09-22 22:37:38 -0300

    Initial commit

---
+++ src/main/java/com/iftm/atividade3/Atividade3Application.java
@@ -0,0 +1,13 @@
+package com.iftm.atividade3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/atividade3/Atividade3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.atividade3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
